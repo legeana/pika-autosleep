84d6c41fb23005aefeb4328be79be021712a832d
Lisa White <legeana@liri.ie>
2024-08-11 18:03:56 +0100

    logconfig: replace simple_logging with env_logger

--- src/logconfig.rs
+++ src/logconfig.rs
@@ -1,14 +1,28 @@
+use std::fs::File;
+
 use anyhow::{anyhow, Context, Result};
 
 pub fn init_cli() -> Result<()> {
-    simple_logging::log_to_stderr(log::LevelFilter::Info);
-    Ok(())
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Info)
+        .default_format()
+        .try_init()
+        .context("failed to build stderr logger")
 }
 
 pub fn init_service() -> Result<()> {
     let exe = std::env::current_exe().context("failed to get current executable")?;
-    let dir = exe.parent().ok_or_else(|| anyhow!("failed to get {exe:?} parent directory"))?;
+    let dir = exe
+        .parent()
+        .ok_or_else(|| anyhow!("failed to get {exe:?} parent directory"))?;
     let log = dir.join("service-log.txt");
-    simple_logging::log_to_file(&log, log::LevelFilter::Info)
-        .with_context(|| format!("failed to log into {log:?}"))
+
+    let target = Box::new(File::create(&log).with_context(|| format!("failed to open {log:?}"))?);
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Info)
+        .default_format()
+        .format_timestamp_secs()
+        .target(env_logger::Target::Pipe(target))
+        .try_init()
+        .with_context(|| format!("failed to build logger into {log:?}"))
 }
